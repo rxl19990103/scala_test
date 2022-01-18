d0acc8a4924b369b6f16fc2b185f93132b1991f5
super_man <1169657809@qq.com>
2022-01-18 23:45:38 +0800

    update scala第一天代码

---
+++ src/scala_demo/test1.scala
@@ -0,0 +1,22 @@
+package scala_demo
+
+/*
+  Object : 关键字，声明一个单例对象（伴生对象）
+ */
+object test1 {
+  /*
+    main 方法：从外部可以直接调用的方法
+    def 方法名称（参数名称：参数类型）：方法返回值类型 = { 方法体 }
+    []：泛型
+   */
+
+
+  def main(args: Array[String]): Unit = {
+
+        val a = 0
+        val b = 99
+        print(a + b)
+
+  }
+
+}
